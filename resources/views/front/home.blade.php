0fed06f64bb76fa7a183f8d5f16b49e722321fdb
Ana AZ <naharyana57@gmail.com>
2018-09-22 16:39:11 +0700

    ganti warna font done

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -110,7 +110,7 @@
       <div class="container-fluid">
         <div class="row">
           @foreach($galeri as $g)
-            <div class="col-lg-4 col-md-4">
+            <div class="col-lg-3 col-md-4">
               <div class="portfolio-item wow fadeInUp">
                 <a href="{{ asset($g->path) }}" class="portfolio-popup">
                   <img src="{{ asset($g->path) }}" alt="">
